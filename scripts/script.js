bc6b7d2d974638d2bf40add9c7ffdd280ba59a71
Luka Franjić <lukafranjic94@gmail.com>
2021-08-12 14:38:34 +0200

    Add sliding logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
+$('#slider-btn-right').click(function () {
+    $('#slider-row-first img:last-child').prependTo('#slider-row-first')
+    $('#slider-row-second img:last-child').prependTo('#slider-row-second')
+});
 
+$('#slider-btn-left').click(function () {
+    $('#slider-row-first img:first-child').appendTo('#slider-row-first')
+    $('#slider-row-second img:first-child').appendTo('#slider-row-second')
+});
