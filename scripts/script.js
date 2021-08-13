097678b4e95ab190c96ebaa569c002dede46841b
Luka Franjić <lukafranjic94@gmail.com>
2021-08-13 15:08:50 +0200

    Refactor slider logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,105 +1,64 @@
 $('#slider-btn-right').click(function () {
-    const lastChildFirst = $('#slider-row-first .slider-image:last-child');
-    const lastChildSecond = $('#slider-row-second .slider-image:last-child');
     const firstRow = $('#slider-row-first .slider-image');
     const secondRow = $('#slider-row-second .slider-image');
-    let firstRowCounter = 0;
-    let secondRowCounter = 0;
-    firstRow.animate({ left: lastChildFirst.width() + 10 }, {
-        complete: function () {
-            firstRowCounter++;
-            if (firstRowCounter === firstRow.length) {
-                $('#slider-row-first .slider-image:last-child').prependTo('#slider-row-first')
-                firstRow.each(function () {
-                    $(this).removeAttr('style');
-                })
-            }
-        }
-    });
-    secondRow.animate({ left: lastChildSecond.width() + 10 }, {
-        complete: function () {
-            secondRowCounter++;
-            if (secondRowCounter === secondRow.length) {
-                $('#slider-row-second .slider-image:last-child').prependTo('#slider-row-second')
-                secondRow.each(function () {
-                    $(this).removeAttr('style');
-                })
-            }
-        }
-    })
+    slideRight(firstRow);
+    slideRight(secondRow);
 });
 
 $('#slider-btn-left').click(function () {
-    const firstChildFirst = $('#slider-row-first .slider-image:first-child');
-    const firstChildSecond = $('#slider-row-second .slider-image:first-child');
     const firstRow = $('#slider-row-first .slider-image');
     const secondRow = $('#slider-row-second .slider-image');
-    let firstRowCounter = 0;
-    let secondRowCounter = 0;
-    let widthSumTop = 0;
-    let widthSumBottom = 0;
-    firstRow.each(function () {
-        widthSumTop += $(this).width();
-    })
-    secondRow.each(function () {
-        widthSumBottom += $(this).width();
-    })
-    const totalLeftTop = widthSumTop + 10 * firstRow.length;
-    const totalLeftBottom = widthSumBottom + 10 * secondRow.length;
-    firstChildFirst.css({ left: totalLeftTop });
-    firstChildSecond.css({ left: totalLeftBottom });
-    firstRow.each(function () {
-        if ($(this).is(":first-child")) {
-            $(this).animate({ left: totalLeftTop - $(this).width() - 10 }, {
-                complete: function () {
-                    firstRowCounter++;
-                    if (firstRowCounter === firstRow.length) {
-                        $('#slider-row-first .slider-image:first-child').appendTo('#slider-row-first')
-                        firstRow.each(function () {
-                            $(this).removeAttr('style');
-                        })
-                    }
-                }
-            });
-        } else {
-            $(this).animate({ right: firstChildFirst.width() + 10 }, {
-                complete: function () {
-                    firstRowCounter++;
-                    if (firstRowCounter === firstRow.length) {
-                        $('#slider-row-first .slider-image:first-child').appendTo('#slider-row-first')
-                        firstRow.each(function () {
-                            $(this).removeAttr('style');
-                        })
-                    }
-                }
-            });
-        }
-    })
-    secondRow.each(function () {
-        if ($(this).is(":first-child")) {
-            $(this).animate({ left: totalLeftBottom - $(this).width() - 10 }, {
-                complete: function () {
-                    secondRowCounter++;
-                    if (secondRowCounter === secondRow.length) {
-                        $('#slider-row-second .slider-image:first-child').appendTo('#slider-row-second')
-                        secondRow.each(function () {
-                            $(this).removeAttr('style');
-                        })
-                    }
-                }
-            });
-        } else {
-            $(this).animate({ right: firstChildSecond.width() + 10 }, {
-                complete: function () {
-                    secondRowCounter++;
-                    if (secondRowCounter === secondRow.length) {
-                        $('#slider-row-second .slider-image:first-child').appendTo('#slider-row-second')
-                        secondRow.each(function () {
-                            $(this).removeAttr('style');
-                        })
-                    }
-                }
-            });
-        }
-    })
+    slideLeft(firstRow);
+    slideLeft(secondRow);
 });
+
+function slideRight(sliderElements) {
+    const lastChild = sliderElements.last();
+    const slider = lastChild.parent();
+    const totalHorizontalMargin = GetTotalHorizontalMargin(lastChild);
+    let counter = 0;
+    sliderElements.animate({ left: lastChild.width() + totalHorizontalMargin }, {
+        complete: () => {
+            counter++;
+            if (counter === sliderElements.length) {
+                lastChild.prependTo(slider);
+                sliderElements.each(function () {
+                    $(this).removeAttr('style');
+                })
+            }
+        }
+    })
+}
+
+function slideLeft(sliderElements) {
+    const firstChild = sliderElements.first();
+    const otherChildren = sliderElements.slice(1);
+    const slider = firstChild.parent();
+    const totalHorizontalMargin = GetTotalHorizontalMargin(firstChild);
+    let counter = 0;
+    let widthSum = 0;
+    sliderElements.each(function () {
+        widthSum += $(this).width();
+    });
+    const totalLeft = widthSum + totalHorizontalMargin * sliderElements.length;
+    firstChild.css({ left: totalLeft });
+    const animateElement = () => {
+        counter++;
+        if (counter === sliderElements.length) {
+            firstChild.appendTo(slider);
+            sliderElements.each(function () {
+                $(this).removeAttr('style');
+            })
+        }
+    }
+    firstChild.animate({ left: totalLeft - firstChild.width() - totalHorizontalMargin }, {
+        complete: animateElement
+    });
+    otherChildren.animate({ right: firstChild.width() + totalHorizontalMargin }, {
+        complete: animateElement
+    });
+}
+
+function GetTotalHorizontalMargin(element) {
+    return parseInt(element.css('marginRight'), 10) + parseInt(element.css('marginLeft'), 10);
+}
