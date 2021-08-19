bb8b47513b3ddfd01676e2801202e7f9a6605ff0
Luka Franjić <lukafranjic94@gmail.com>
2021-08-19 13:54:44 +0200

    Wrap javascript code in .ready function

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,53 @@
-$('#slider-btn-right').click(function () {
-    const firstRow = $('#slider-row-first .slider-image');
-    const secondRow = $('#slider-row-second .slider-image');
+$(document).ready(function () {
+    $('#slider-btn-right').click(function () {
+        const firstRow = $('#slider-row-first .slider-image');
+        const secondRow = $('#slider-row-second .slider-image');
 
-    slideRight(firstRow);
-    slideRight(secondRow);
-});
-
-$('#slider-btn-left').click(function () {
-    const firstRow = $('#slider-row-first .slider-image');
-    const secondRow = $('#slider-row-second .slider-image');
-
-    slideLeft(firstRow);
-    slideLeft(secondRow);
-});
-
-function slideRight(sliderElements) {
-    const lastChild = sliderElements.last();
-    const slider = lastChild.parent();
-    const totalHorizontalMargin = getTotalHorizontalMargin(lastChild);
-
-    sliderElements.animate({ left: lastChild.width() + totalHorizontalMargin }).promise().then(() => {
-        lastChild.prependTo(slider);
-        sliderElements.each((i, elem) => {
-            $(elem).removeAttr('style');
-        });
+        slideRight(firstRow);
+        slideRight(secondRow);
     });
-}
 
-function slideLeft(sliderElements) {
-    const firstChild = sliderElements.first();
-    const otherChildren = sliderElements.slice(1);
-    const slider = firstChild.parent();
-    const totalHorizontalMargin = getTotalHorizontalMargin(firstChild);
-    const widthSum = sliderElements.map((i, elem) => $(elem).width()).toArray().reduce((accumulator, currentValue) => accumulator + currentValue);
-    const totalLeft = widthSum + totalHorizontalMargin * sliderElements.length;
+    $('#slider-btn-left').click(function () {
+        const firstRow = $('#slider-row-first .slider-image');
+        const secondRow = $('#slider-row-second .slider-image');
 
-    firstChild.css({ left: totalLeft });
-    firstChild.animate({ left: totalLeft - firstChild.width() - totalHorizontalMargin });
-    otherChildren.animate({ right: firstChild.width() + totalHorizontalMargin });
-    sliderElements.promise().then(() => {
-        firstChild.appendTo(slider);
-        sliderElements.each((i, elem) => {
-            $(elem).removeAttr('style');
-        });
+        slideLeft(firstRow);
+        slideLeft(secondRow);
     });
-}
 
-function getTotalHorizontalMargin(element) {
-    return parseInt(element.css('marginRight'), 10) + parseInt(element.css('marginLeft'), 10);
-}
+    function slideRight(sliderElements) {
+        const lastChild = sliderElements.last();
+        const slider = lastChild.parent();
+        const totalHorizontalMargin = getTotalHorizontalMargin(lastChild);
+
+        sliderElements.animate({ left: lastChild.width() + totalHorizontalMargin }).promise().then(() => {
+            lastChild.prependTo(slider);
+            sliderElements.each((i, elem) => {
+                $(elem).removeAttr('style');
+            });
+        });
+    }
+
+    function slideLeft(sliderElements) {
+        const firstChild = sliderElements.first();
+        const otherChildren = sliderElements.slice(1);
+        const slider = firstChild.parent();
+        const totalHorizontalMargin = getTotalHorizontalMargin(firstChild);
+        const widthSum = sliderElements.map((i, elem) => $(elem).width()).toArray().reduce((accumulator, currentValue) => accumulator + currentValue);
+        const totalLeft = widthSum + totalHorizontalMargin * sliderElements.length;
+
+        firstChild.css({ left: totalLeft });
+        firstChild.animate({ left: totalLeft - firstChild.width() - totalHorizontalMargin });
+        otherChildren.animate({ right: firstChild.width() + totalHorizontalMargin });
+        sliderElements.promise().then(() => {
+            firstChild.appendTo(slider);
+            sliderElements.each((i, elem) => {
+                $(elem).removeAttr('style');
+            });
+        });
+    }
+
+    function getTotalHorizontalMargin(element) {
+        return parseInt(element.css('marginRight'), 10) + parseInt(element.css('marginLeft'), 10);
+    }
+});
