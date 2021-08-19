42145df23a458e763f61f32c02e7d52978102b97
Luka Franjić <lukafranjic94@gmail.com>
2021-08-19 15:36:55 +0200

    Use .outerWidth() method instead of calculating it manually and simplify totalLeft logic

--- scripts/script.js
+++ scripts/script.js
@@ -19,9 +19,8 @@ $(document).ready(() => {
     const slideRight = (sliderElements) => {
         const lastChild = sliderElements.last();
         const slider = lastChild.parent();
-        const totalHorizontalMargin = getTotalHorizontalMargin(lastChild);
 
-        sliderElements.animate({ left: lastChild.width() + totalHorizontalMargin }).promise().then(() => {
+        sliderElements.animate({ left: lastChild.outerWidth(true) }).promise().then(() => {
             lastChild.prependTo(slider);
             sliderElements.each((i, elem) => {
                 $(elem).removeAttr('style');
@@ -33,22 +32,16 @@ $(document).ready(() => {
         const firstChild = sliderElements.first();
         const otherChildren = sliderElements.slice(1);
         const slider = firstChild.parent();
-        const totalHorizontalMargin = getTotalHorizontalMargin(firstChild);
-        const widthSum = sliderElements.map((i, elem) => $(elem).width()).toArray().reduce((accumulator, currentValue) => accumulator + currentValue);
-        const totalLeft = widthSum + totalHorizontalMargin * sliderElements.length;
+        const totalLeft = slider.width();
 
         firstChild.css({ left: totalLeft });
-        firstChild.animate({ left: totalLeft - firstChild.width() - totalHorizontalMargin });
-        otherChildren.animate({ right: firstChild.width() + totalHorizontalMargin });
+        firstChild.animate({ left: totalLeft - firstChild.outerWidth(true) });
+        otherChildren.animate({ right: firstChild.outerWidth(true) });
         sliderElements.promise().then(() => {
             firstChild.appendTo(slider);
             sliderElements.each((i, elem) => {
                 $(elem).removeAttr('style');
             });
         });
-    };
-
-    const getTotalHorizontalMargin = (element) => {
-        return parseInt(element.css('marginRight'), 10) + parseInt(element.css('marginLeft'), 10);
     };
 });
